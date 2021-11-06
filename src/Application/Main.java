b2702841df6e9af4a35de6d84c2644c621c45ca5
Vitor Lira <vitorlirasti@hotmail.com>
2021-11-06 12:11:34 -0300

    Seller entity class

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,6 +1,11 @@
 package Application;
 
+
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
@@ -9,6 +14,8 @@ public class Main {
 		Department obj = new Department(2,"books");
 		System.out.println(obj);
 		
+		Seller seller = new Seller(21,"bob","bob@hot",new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
