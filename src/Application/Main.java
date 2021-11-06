a897560febd006ea8b73b7dfd068b7fff53904dd
Vitor Lira <vitorlirasti@hotmail.com>
2021-11-06 18:19:48 -0300

    Ending the departmentDao

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -15,6 +16,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: seller FindById ===");
@@ -49,7 +51,7 @@ public class Main {
 	
 		System.out.println();
 		
-		System.out.println("=== TEST 5: seller Insert ===");
+		System.out.println("=== TEST 5: seller Update ===");
 		seller = sellerDao.fidnById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
@@ -57,13 +59,36 @@ public class Main {
 	
 		System.out.println();
 		
-		System.out.println("=== TEST 6: seller Insert ===");
+		System.out.println("=== TEST 6: seller Delete ===");
 		System.out.println("Enter id for delete test: ");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
 		System.out.println("Delete completed");
 		
-		sc.close();
+		System.out.println();
+		System.out.println("----------DEPARTMENT----------");
+		System.out.println();
+		
+		System.out.println("=== TEST 7: department insert ===");
+		DepartmentDao departmentDao =  DaoFactory.createDepartmentDao();
+		List<Department> listD;
+		Department d = new Department(null, "accessories");
+		departmentDao.insert(d);
+		System.out.println("Inserted New id: "+d.getId());
+		
+		
+		System.out.println();
+		System.out.println("=== TEST 8: department findById ===");
+		d = departmentDao.fidnById(2);
+		System.out.println(d);
+
+		System.out.println();
+		System.out.println("=== TEST 9: department findAll ===");
+		listD = departmentDao.findAll();
+		for(Department dep : listD) {
+			System.out.println(dep);
+		}
+	
 	}
 
 }
