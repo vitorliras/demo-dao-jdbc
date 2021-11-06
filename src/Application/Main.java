83679a01232b29e0c83925978e6362c97b2dcc82
Vitor Lira <vitorlirasti@hotmail.com>
2021-11-06 11:34:36 -0300

    Department entity class

---
+++ src/Application/Main.java
@@ -0,0 +1,14 @@
+package Application;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(2,"books");
+		System.out.println(obj);
+		
+	}
+
+}
